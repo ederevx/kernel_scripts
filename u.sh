5b3c679ba4d92237ee41e41a495c0ae2f0db8035
ederekun <sedrickvince@gmail.com>
2021-10-16 02:22:42 +0300

    scripts: Separate update_linux from update_repo and add more repositories
    Signed-off-by: ederekun <sedrickvince@gmail.com>

--- u.sh
+++ u.sh
@@ -9,22 +9,16 @@ decho "Executing update script..."
 
 param_func "$@"
 
-update_linux
-
 if [[ $1 == "-x" ]]; then
-	exit 0
-fi
-
-if [ -d $REL_DIR ]; then
+	update_linux
+else
 	update_repo $REL_DIR
-fi
-
-if [ -d $SRT_DIR ]; then
 	update_repo $SRT_DIR
-fi
-
-if [ -d $ZIP_DIR ]; then
 	update_repo $ZIP_DIR
+	update_repo $PACK_DIR
+	update_repo $CLANG_DIR
+	update_repo ${GCC_DIR}64
+	update_repo ${GCC_DIR}32
 fi
 
 exit 0
